5d151cbe48d82f1cf70eac3597bd50ab88713487
Ahmed M. Elhalaby <Eng.Ahmed.Elhalaby@gmail.com>
2021-04-06 05:43:58 +0300

    update

--- dist/js/about.e75f391e.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,n,e){"use strict";e.r(n);var a=e("f2bf"),c={class:"about"},o=Object(a["h"])("h1",null,"This is an about page",-1);function s(t,n){return Object(a["t"])(),Object(a["d"])("div",c,[o])}const u={};u.render=s;n["default"]=u}}]);
-//# sourceMappingURL=about.e75f391e.js.map
